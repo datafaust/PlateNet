290aab4d2e1d81443cc188c9cd0aa317a6924669
mac <mac@macs-MBP.fios-router.home>
2020-06-07 11:32:51 -0400

    remove caddy etc

--- nodetest/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 4000
-
-app.get('/', (req, res) => res.send('this is testing...!'))
-
-app.listen(port, () => console.log(`running on port 4000`))
